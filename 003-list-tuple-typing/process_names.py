d41a205b3fce5837c4471dbc70159f5bd5b54543
shafahadshah <pirshahfahadshah@gmail.com>
2026-08-09 10:40:07 +0500

    function accepting list[str]

---
+++ 003-list-tuple-typing/process_names.py
@@ -0,0 +1,12 @@
+def process_names(names: list[str]) -> None:
+    for name in names:
+        print(name)
+
+
+users: list[str] = [
+    "Alice",
+    "Bob",
+    "Charlie",
+]
+
+process_names(users)
