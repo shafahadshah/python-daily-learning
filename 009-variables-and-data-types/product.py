8180be3769f201d91c5ffce204a77ae80ad50abc
shafahadshah <pirshahfahadshah@gmail.com>
2026-08-15 10:06:26 +0500

    product informatio

---
+++ 009-variables-and-data-types/product.py
@@ -0,0 +1,9 @@
+product_name: str = "Laptop"
+product_id: int = 101
+product_price: float = 999.99
+in_stock: bool = True
+
+print("Product:", product_name)
+print("ID:", product_id)
+print("Price:", product_price)
+print("In stock:", in_stock)
