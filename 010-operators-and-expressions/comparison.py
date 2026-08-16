dfe924df571559f9570a56e7e7abb829a2469e6e
shafahadshah <pirshahfahadshah@gmail.com>
2026-08-16 10:37:42 +0500

    comparison operators

---
+++ 010-operators-and-expressions/comparison.py
@@ -0,0 +1,9 @@
+age: int = 20
+required_age: int = 18
+
+print(age == required_age)
+print(age != required_age)
+print(age > required_age)
+print(age < required_age)
+print(age >= required_age)
+print(age <= required_age)
